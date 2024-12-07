d3e777bbf4f98cfc636db972cb15a008fa5d2a02
Moneodev <95101001+SeeminglyUnrelated@users.noreply.github.com>
2024-12-07 16:12:42 -0500

    Implementing tilemaps
    Minor brain concussion

---
+++ assets/TestTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="TestTileset" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="Tilemap/Modern_Interiors_Free_v2.2/Modern tiles_Free/Old/Tileset_32x32_1.png" width="320" height="320"/>
+</tileset>
